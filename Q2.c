190b5d3208ce67e974784f94211e35ca5d9247e3
Roshnithgupta <162914726+Roshnithgupta@users.noreply.github.com>
2026-08-31 23:58:22 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,8 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include<stdio.h>
+
+int main() {
+    int a=8, b=2;
+    printf("%d, %d, %d, %d", a+b, a-b, a*b, a/b);
+    return 0;
+}
